70f1fdafc2b99124289f39636c1f2060f68e313d
EmbraceLife <1227561934@qq.com>
2015-10-18 11:08:38 +0800

    7.4 p5 handling DOM with callbacks

--- 7.4 p5.js- Handling DOM events with callbacks /sketch.js
+++ 7.4 p5.js- Handling DOM events with callbacks /sketch.js
@@ -1,7 +1,31 @@
+// use createButton to add button on page
+// also use mousePressed function to call actions
+
+var bgcolor;
+var button;
+
+
 function setup() {
+  canvas = createCanvas(200, 200);
+  bgcolor = color(200);
+  button = createButton("go go go go");
+  button.mousePressed(changeColor);
   
 }
 
-function draw() {
+function changeColor() {
+  bgcolor = color(random(255))
   
+}
+
+// function mousePressed() {
+//   changeColor();
+// }
+
+
+function draw() {
+  background(bgcolor);
+  fill(255, 0, 175);
+  rect(100, 100, 50, 50);
+
 }
